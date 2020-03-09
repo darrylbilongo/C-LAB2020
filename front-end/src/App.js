32d62101ee7febf3c9562cec9d386fd201506752
Bouquoyoue Bilel <HE201666@students.ephec.be>
2020-03-10 00:21:01 +0100

    Front-end page de login
    Idée de page de login pour commencer. Ne pas oublier d'améliorer le css.

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PageLogin from "./components/PageLogin"
+import './App.css';
+
+class App extends Component {
+
+  render(){
+    return (
+        <div className="App">
+          <PageLogin></PageLogin>
+        </div>
+    );
+  }
+  
+    
+}
+
+export default App;
---
+++ front-end/src/components/PageLogin.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Formulaire from "./Formulaire"
+
+class PageLogin extends Component{
+    render(){
+        return (
+            <div>
+                <h1>C-LAB</h1>
+            
+                <Formulaire></Formulaire> 
+            </div>
+        )
+    }
+}
+
+export default PageLogin;
