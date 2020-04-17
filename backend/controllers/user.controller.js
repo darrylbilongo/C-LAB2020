1ccea8f83a46cc3e632a9c0a005b774d69473ace
darrylbilongo <darrylbilongo@gmail.com>
2020-04-17 17:12:31 +0200

    commit put

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -119,3 +119,27 @@ exports.getUser = (req, res) => {
             })
         });
 };
+
+exports.updateUser = (req, res) => {
+    const id = req.params.id;
+
+    User.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+        if (num == 1) {
+            res.send({
+            message: "Tutorial was updated successfully."
+            });
+        } else {
+            res.send({
+            message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+            });
+        }
+        })
+        .catch(err => {
+        res.status(500).send({
+            message: "Error updating Tutorial with id=" + id
+        });
+        });
+}
