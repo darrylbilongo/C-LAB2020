4bea296e888c42f334f531fcc48b575ab0090fd9
Souhaïb Azzouz <sou_sou_azzouz@hotmail.com>
2020-04-19 15:09:09 +0200

    Ajout des commentaires swagger model

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,3 +1,18 @@
+/**
+ * @swagger
+ * definitions:
+ *  post:
+ *    type: object
+ *    properties:
+ *      title:
+ *        type: string
+ *      description:
+ *        type: string
+ *      published:
+ *        type: boolean
+ * 
+ */
+
 module.exports = (sequelize, Sequelize) => {
   const Post = sequelize.define("post", {
     title: {
