ab374c76c7750fc41880141728ae4efd3b045969
souhaibazzouz <45404921+souhaibazzouz@users.noreply.github.com>
2020-05-13 12:17:36 +0200

    Delete opinion.js

--- backend/routes/opinion.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const opinions = require("../controllers/opinion.controller.js");
-
-router.post('/', opinions.create );
-
-router.post('/get/:id', opinions.findAll);
-
-module.exports = router;
