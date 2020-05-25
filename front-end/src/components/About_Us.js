e0a974c121db905f00bd25e34b8121b784840455
Bouquoyoue Bilel <HE201666@students.ephec.be>
2020-05-26 01:22:51 +0200

    Changement complet de About_us en contact support

--- front-end/src/components/About_Us.js
+++ front-end/src/components/About_Us.js
@@ -2,57 +2,65 @@ import React, { Component } from "react";
 import Souhaib from '../images/souhaib.png'
 import Darryl from '../images/darryl.png'
 import Bilel from '../images/bilel.png'
+import Carousel from 'react-bootstrap/Carousel'
 
 class About_Us extends Component{
     render(){
         return(
-            <div className="justify-content-center">
-                <div className="description2">
-                    <p>La musique est un art où bien souvent les plus grands sont bien au dessus et où il est très difficile de se faire une place.</p>
-                    <p>C'est le constat qu'on fait 3 étudiants avant de se lancer dans la création de <span className="text-danger font-weight-bold">"C-LAB"</span></p> 
-                    C-Lab est un projet qui a pour but d'aider les musiciens en manque de visibilité à se faire remarquer par d'autres personnes issues de ce domaine.
-                </div>
+            <div className="aproposTot">
+                
+
                 <div className="apropos">
-                    <div className="">
-                        <div className="col">
-                            <a href='https://www.linkedin.com/in/souha%C3%AFb-azzouz-a92b67198/' target="_blank">
-                            <img 
-                                className="aproposImg d-inline"
+                    <h1 className="blanco2">Nos membres</h1>
+                    <Carousel >
+                        <Carousel.Item>
+                        <a href='https://www.linkedin.com/in/souha%C3%AFb-azzouz-a92b67198/' target="_blank">
+                            <img
+                                className="img-carousel"
                                 src={Souhaib}
-                                alt="Azzouz Souhaïb"/></a>
-                                <span className="aproposText">
-                                    <h3>Azzouz Souhaïb</h3>
-                                    <p>Designer et communication</p>
-                                </span>
-                        </div>
-                    </div>
-                    <div className="col">
+                                alt="Azzouz Souhaïb"
+                            />
+                            <Carousel.Caption>
+                                <h3>Azzouz Souhaïb</h3>
+                                <p>Designer et communication</p>
+                            </Carousel.Caption>
+                        </a>
+                            </Carousel.Item>
+                        <Carousel.Item>
                         <a href='https://www.linkedin.com/in/darrylbilongo/' target="_blank">
-                        <img 
-                            className="aproposImg d-inline"
-                            src={Darryl}
-                            alt="Bilongo Darryl"/></a>
-                            <span className="aproposText">
+                            <img
+                                className="img-carousel"
+                                src={Darryl}
+                                alt="Bilongo Darryl"
+                            />
+
+                            <Carousel.Caption>
                                 <h3>Bilongo Darryl</h3>
                                 <p>Gestionnaire de données</p>
-                            </span>
-                    </div>
-                    <div className="col">
+                            </Carousel.Caption>
+                        </a>
+                        </Carousel.Item>
+                        <Carousel.Item>
                         <a href='https://www.linkedin.com/in/bilel-bouquoyoue-700763134/' target="_blank">
-                        <img 
-                            className="aproposImg"
-                            src={Bilel}
-                            alt="Bouqoyoue Bilel"/></a>
-                            <span className="aproposText">
+                            <img
+                                className="img-carousel"
+                                src={Bilel}
+                                alt="Bouquoyoue Bilel"
+                            />
+
+                            <Carousel.Caption>
                                 <h3>Bouquoyoue Bilel</h3>
                                 <p>L'expert Musical</p>
-                            </span>
-                    </div>
+                            </Carousel.Caption>
+                        </a>
+                        </Carousel.Item>
+                        
+                    </Carousel>
                 </div>
-                <div>
-                    <p className="text-white">Si vous souhaitez prendre contact avec un membre du projet pour une amélioration ou un retour, choisissez votre interlocuteur en cliquant sur sa photo</p>
+                <div className="contact">
+                    <h5 className="text-white">Si vous souhaitez prendre contact avec un membre du projet pour une amélioration ou un retour, choisissez votre interlocuteur en cliquant sur sa photo</h5>
                 </div>
-                <div>
+                <div className="basabout">
                     <a className="btn btn-primary btn-block btn-lg"
                         href="mailto:sou_sou_azzouz@hotmail.com"
                         data-toggle="tooltip"
