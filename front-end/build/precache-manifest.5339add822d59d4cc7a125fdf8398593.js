c974c288fa64d489e6c085589609f696d8c74fc3
Souhaïb Azzouz <sou_sou_azzouz@hotmail.com>
2020-05-27 15:10:10 +0200

    setup

---
+++ front-end/build/precache-manifest.5339add822d59d4cc7a125fdf8398593.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8dddcf708ef9e38471289466dfd756c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f20f36ed64169a2bd34f",
+    "url": "/static/css/2.28c4475d.chunk.css"
+  },
+  {
+    "revision": "2ff46e2ce38e4e8c5b3b",
+    "url": "/static/css/main.ad84b78a.chunk.css"
+  },
+  {
+    "revision": "f20f36ed64169a2bd34f",
+    "url": "/static/js/2.eba23e93.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.eba23e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff46e2ce38e4e8c5b3b",
+    "url": "/static/js/main.30aed07a.chunk.js"
+  },
+  {
+    "revision": "5130b3c4bbba431521b4",
+    "url": "/static/js/runtime-main.d5d88daf.js"
+  },
+  {
+    "revision": "f2a9dd069f75cfabbf6af536b8b97f6c",
+    "url": "/static/media/Clab.f2a9dd06.png"
+  },
+  {
+    "revision": "e229194b10ec2bcf59202ecb4bfcb09b",
+    "url": "/static/media/Double_Diamond.e229194b.png"
+  },
+  {
+    "revision": "34ada1638ef7bef370e593e45e2cd29a",
+    "url": "/static/media/Insta.34ada163.png"
+  },
+  {
+    "revision": "ad874c9d38ef3c046b60eed924700d04",
+    "url": "/static/media/Micro_edited.ad874c9d.jpg"
+  },
+  {
+    "revision": "6113728a5a6e38d834f9013facd7c56c",
+    "url": "/static/media/Twitter_Logo.6113728a.png"
+  },
+  {
+    "revision": "16d09e4c781147d69e1268da5e8595e5",
+    "url": "/static/media/bilel.16d09e4c.png"
+  },
+  {
+    "revision": "913d19b5493b06d4b7adb7cac66c6d13",
+    "url": "/static/media/darryl.913d19b5.png"
+  },
+  {
+    "revision": "5aa974ff4723605a3557ae9affd9c7e2",
+    "url": "/static/media/souhaib.5aa974ff.png"
+  },
+  {
+    "revision": "dcb16337d2cac35efcc605a142e49945",
+    "url": "/static/media/youtube.dcb16337.png"
+  }
+]);
