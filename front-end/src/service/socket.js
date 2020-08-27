a084a1bf27aadc21114f23089a614333076f547c
darrylbilongo <d.bilongo@students.ephec.be>
2020-08-27 23:08:54 +0200

    new version front

--- front-end/src/service/socket.js
+++ front-end/src/service/socket.js
@@ -1,7 +1,5 @@
 import io from "socket.io-client";
-
-export const socket = io.connect('https://darrylbilongo.site/clab', {
-    // WARNING: in that case, there is no fallback to long-polling
-    transports: [ 'websocket' ] // or [ 'websocket', 'polling' ], which is the same thing
+export const socket = io.connnect({
+    transports: [ 'websocket', 'polling' ]
 })
 
