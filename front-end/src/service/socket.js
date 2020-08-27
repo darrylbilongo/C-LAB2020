962961723e986b786bdd14d18ecc6cdf468f4642
darrylbilongo <d.bilongo@students.ephec.be>
2020-08-27 22:51:57 +0200

    bon client socket

--- front-end/src/service/socket.js
+++ front-end/src/service/socket.js
@@ -1,4 +1,7 @@
 import io from "socket.io-client";
 
-export const socket = io.connect('https://darrylbilongo.site/clab')
+export const socket = io.connect('https://darrylbilongo.site/clab', {
+    // WARNING: in that case, there is no fallback to long-polling
+    transports: [ 'websocket' ] // or [ 'websocket', 'polling' ], which is the same thing
+})
 
